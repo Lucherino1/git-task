6b5fe51ac7363f334e5a4aae457d691360715e45
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-06-07 16:04:21 +0300

    Created first test user

--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,17 @@
 const str: string = "abobus";
 console.log(str);
+
+const firstCommit: string = "Sosantos";
+console.log(firstCommit);
+
+interface Sosi {
+  name: string;
+  age: number;
+}
+
+const user: Sosi = {
+  name: "oleg",
+  age: 42,
+};
+
+console.log(user);
